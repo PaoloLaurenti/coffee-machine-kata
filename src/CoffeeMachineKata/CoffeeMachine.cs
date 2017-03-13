ab440d5863da3808ebc56e207478e89f2b5238d8
Paolo Laurenti <laurentipaolo@gmail.com>
2017-03-13 14:04:16 +0100

    Refactoring: repace if in CoffeeMachine with dictionary

--- src/CoffeeMachineKata/CoffeeMachine.cs
+++ src/CoffeeMachineKata/CoffeeMachine.cs
@@ -1,20 +1,25 @@
+using System.Collections.Generic;
+
 namespace CoffeeMachineKata
 {
     public class CoffeeMachine
     {
         private readonly IDrinkMaker _drinkMaker;
+        private readonly Dictionary<BeverageType, string> _drinkMakerCommands;
 
         public CoffeeMachine(IDrinkMaker drinkMaker)
         {
             _drinkMaker = drinkMaker;
+            _drinkMakerCommands = new Dictionary<BeverageType, string>
+            {
+                {BeverageType.Tea, "T::" },
+                {BeverageType.Coffee, "C::" },
+            };
         }
 
         public void Dispense(Beverage beverage)
         {
-            if (beverage.Type == BeverageType.Tea)
-                _drinkMaker.Make("T::");
-            else if (beverage.Type == BeverageType.Coffee)
-                _drinkMaker.Make("C::");
+            _drinkMaker.Make(_drinkMakerCommands[beverage.Type]);
         }
     }
 }
