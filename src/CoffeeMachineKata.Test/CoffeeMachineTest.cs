44d30b288a7960b7bf92a786a30f47d8c43fd7f9
Paolo Laurenti <laurentipaolo@gmail.com>
2017-03-13 14:09:41 +0100

    CoffeeMachineTest refactoring

--- src/CoffeeMachineKata.Test/CoffeeMachineTest.cs
+++ src/CoffeeMachineKata.Test/CoffeeMachineTest.cs
@@ -5,31 +5,35 @@ namespace CoffeeMachineKata.Test
 {
     public class CoffeeMachineTest
     {
+        private readonly DrinkMakerSpy _drinkMakerSpy;
+        private readonly CoffeeMachine _coffeeMachine;
+
+        public CoffeeMachineTest()
+        {
+            _drinkMakerSpy = new DrinkMakerSpy();
+            _coffeeMachine = new CoffeeMachine(_drinkMakerSpy);
+        }
+
         [Theory]
         [InlineData(BeverageType.Tea, "T::")]
         [InlineData(BeverageType.Coffee, "C::")]
         [InlineData(BeverageType.Chocolate, "H::")]
         public void MakeBeverageWithNoSugar(BeverageType type, string expectedCommand)
         {
-            var drinkMakerSpy = new DrinkMakerSpy();
-            var coffeeMachine = new CoffeeMachine(drinkMakerSpy);
+            _coffeeMachine.Dispense(new Beverage(type, 0));
 
-            coffeeMachine.Dispense(new Beverage(type, 0));
-
-            drinkMakerSpy.CommandsReceived.ShouldAllBeEquivalentTo(new[] {expectedCommand});
+            _drinkMakerSpy.CommandsReceived.ShouldAllBeEquivalentTo(new[] {expectedCommand});
         }
 
+        [Fact]
         public void MakeMoreBeverages()
         {
-            var drinkMakerSpy = new DrinkMakerSpy();
-            var coffeeMachine = new CoffeeMachine(drinkMakerSpy);
+            _coffeeMachine.Dispense(new Beverage(BeverageType.Coffee, 0));
+            _coffeeMachine.Dispense(new Beverage(BeverageType.Tea, 0));
+            _coffeeMachine.Dispense(new Beverage(BeverageType.Chocolate, 0));
+            _coffeeMachine.Dispense(new Beverage(BeverageType.Tea, 0));
 
-            coffeeMachine.Dispense(new Beverage(BeverageType.Coffee, 0));
-            coffeeMachine.Dispense(new Beverage(BeverageType.Tea, 0));
-            coffeeMachine.Dispense(new Beverage(BeverageType.Chocolate, 0));
-            coffeeMachine.Dispense(new Beverage(BeverageType.Tea, 0));
-
-            drinkMakerSpy.CommandsReceived.ShouldAllBeEquivalentTo(new[] { "C::", "T::", "H::", "T::" });
+            _drinkMakerSpy.CommandsReceived.ShouldAllBeEquivalentTo(new[] { "C::", "T::", "H::", "T::" });
         }
     }
 }
